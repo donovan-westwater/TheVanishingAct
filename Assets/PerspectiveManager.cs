3c3bd739e41a91f2ff8d2d05831df65c67ed9658
donovan-westwater <Donovan.westwater@gmail.com>
2019-10-08 00:50:48 -0400

    Started creating the teleporation tunnel. Rotation and position of the tunnel are not functioning properly it.

---
+++ Assets/PerspectiveManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerspectiveManager : MonoBehaviour
+{
+    public GameObject tunnel;
+    bool isOn = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tunnel = GameObject.Find("SpatialTunnel").gameObject;
+        tunnel.SetActive(false);
+        GameObject[] gameObjectArray = Resources.FindObjectsOfTypeAll<GameObject>();
+        // GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("enemy");
+        foreach (GameObject go in gameObjectArray)
+        {
+            if (go.CompareTag("Wall") || go.CompareTag("enemy") || go.CompareTag("Glass"))
+            {
+                go.SetActive(true);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isOn)
+        {
+            isOn = true;
+            tunnel.SetActive(true);
+            GameObject[] gameObjectArray = Resources.FindObjectsOfTypeAll<GameObject>();
+            // GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("enemy");
+            foreach (GameObject go in gameObjectArray)
+            {
+                if (go.CompareTag("Wall") || go.CompareTag("enemy")||go.CompareTag("Glass"))
+                {
+                    go.SetActive(false);
+                }
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.Space) && isOn)
+        {
+            isOn = false;
+            tunnel.SetActive(false);
+            GameObject[] gameObjectArray = Resources.FindObjectsOfTypeAll<GameObject>();
+           // GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("enemy");
+            foreach (GameObject go in gameObjectArray)
+            {
+                if (go.CompareTag("Wall") || go.CompareTag("enemy")||go.CompareTag("Glass"))
+                {
+                    go.SetActive(true);
+                }
+            }
+            
+        }
+    }
+}
---
+++ Assets/TunnelLogic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelLogic : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").gameObject;
+        this.transform.position = player.transform.position;
+        //Vector2 dir = player.GetComponent<Player_Controls>().grabSpellAim();
+        //float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        //this.transform.Rotate(new Vector3(0,0,1),angle);
+        
+    }
+    private void OnEnable()
+    {
+        print(transform.position);
+        player = GameObject.Find("Player").gameObject;
+        //this.transform.position = player.transform.position;
+        Vector2 dir = player.GetComponent<Player_Controls>().grabSpellAim();
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        this.transform.Rotate(new Vector3(0,0,1),angle);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
