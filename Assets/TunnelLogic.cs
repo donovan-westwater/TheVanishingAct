c68b6dbff0aca8061a027a4fdb6ff2e89f33cc23
donovan-westwater <Donovan.westwater@gmail.com>
2019-10-09 23:33:33 -0400

    Fixed part of the rotation issue. It was rotating around the pivot, which wasn't centered on the player. Changed rotation to a world rotate to fix it. The amount of rotation is not quite right however, and needs to be fixed.

--- Assets/TunnelLogic.cs
+++ Assets/TunnelLogic.cs
@@ -5,28 +5,55 @@ using UnityEngine;
 public class TunnelLogic : MonoBehaviour
 {
     GameObject player;
+    Vector2 tunnelDir;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player").gameObject;
+        tunnelDir = player.GetComponent<Player_Controls>().grabSpellAim();
         this.transform.position = player.transform.position;
         //Vector2 dir = player.GetComponent<Player_Controls>().grabSpellAim();
         //float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         //this.transform.Rotate(new Vector3(0,0,1),angle);
         
     }
+    //Angle gets out of sync when crossing to other side since only the shortest angle is being looked at!
     private void OnEnable()
-    {
-        print(transform.position);
+    {  
+        
+       
+        //get cross product to determine turn direction and get magitude of angle to turn to correct place
+        
+       // print("Start: "+transform.position);
         player = GameObject.Find("Player").gameObject;
-        //this.transform.position = player.transform.position;
+       // player.GetComponent<Collider2D>().enabled = false;
+        this.transform.position = player.transform.position;
         Vector2 dir = player.GetComponent<Player_Controls>().grabSpellAim();
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        this.transform.Rotate(new Vector3(0,0,1),angle);
+        float angle = Vector2.Angle(tunnelDir, dir);
+        tunnelDir = dir;
+        if (Vector3.Cross(dir.normalized, tunnelDir.normalized).z < 0 )
+        {
+            angle =  Vector2.Angle(tunnelDir, dir);
+            //0.1f
+        }
+        else if (Vector3.Cross(dir.normalized, tunnelDir.normalized).z > 0)
+        {
+             angle= -Vector2.Angle(tunnelDir, dir);
+            //-0.1f
+        }
+        this.transform.Rotate(new Vector3(0, 0, 1), angle, Space.World);
+        //float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        //this.transform.RotateAround(player.transform.position, new Vector3(0, 0, 1), angle);
+        //player.GetComponent<Collider2D>().enabled = true;
+        //print("End: " + transform.position);
+
+
     }
     // Update is called once per frame
     void Update()
     {
+       // player = GameObject.Find("Player").gameObject;
         
+        //this.transform.Rotate(new Vector3(0, 0, 1), 0.1f,Space.World); this one works
     }
 }
