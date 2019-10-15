b54062a71bcb4a05d4ba475ca62c0b2780ccabb2
donovan-westwater <Donovan.westwater@gmail.com>
2019-10-14 23:33:35 -0400

    Setting up the interactibles in unity. First making the trigger interactible base class and then all its children. Then make mana orb. Then turn everything into prefabs and start making level system!

--- Assets/alarm.cs
+++ Assets/alarm.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class alarm : MonoBehaviour
 {
+    public GameObject[] attachedTriggers = new GameObject[4];
     public float alertRadis;
     public float triggerRadis;
-    Transform lastSeen;
+    Vector2 lastSeen;
+    bool alarmTriggered = false;
+    float timer = 0;
+    float waitTime = 4f;
     //bool sentAlert = false;
-    
-    new void Start()
+
+    void Start()
     {
-       // base.Start();
-        lastSeen = GameObject.Find("Player").transform;
+        // base.Start();
+        lastSeen = GameObject.Find("Player").transform.position;
         alertRadis = 20f;
         triggerRadis = 10f;
     }
@@ -21,22 +25,42 @@ public class alarm : MonoBehaviour
     void Update()
     {
         GameObject player = GameObject.Find("Player");
+        //Change to a raycast system! [move this over to camera script]
+        //have a compare tag system for the interactibles, all should have a triggered setting.
+        //use OOD for interactibles that trigger the alarm!!
         if (Vector2.Distance(player.transform.position, this.transform.position) < triggerRadis)
         {
-            lastSeen = player.transform;
+            lastSeen = player.transform.position;
+            alarmTriggered = true;
+        }
+        if (alarmTriggered)
+        {
+            timer += 1 * Time.deltaTime;
+            alertGuards();
+            if(timer > waitTime)
+            {
+                alarmTriggered = false;
+                timer = 0;
+            }
         }
     }
-    void alertGuards()
+    bool alertGuards()
     {
+        bool ret = false;
+
+        GameObject gameout = new GameObject();
         GameObject[] guards = GameObject.FindGameObjectsWithTag("enemy");
         foreach (GameObject g in guards)
         {
-            if (Vector2.Distance(g.transform.position, this.transform.position) < alertRadis)
+            if (g.transform != this.transform && Vector2.Distance(g.transform.position, this.transform.position) < alertRadis)
             {
-
+                ret = true;
+                g.GetComponent<BasicAi>().setAlert(true);
+                gameout.transform.position = lastSeen;
+                g.GetComponent<BasicAi>().setWhereToSearch(gameout.transform);
             }
         }
 
-
+        return ret;
     }
 }
