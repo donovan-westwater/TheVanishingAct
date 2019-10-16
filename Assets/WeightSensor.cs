c90bca1a4b299c3a7a05ea6bfb50fa316593fff4
donovan-westwater <Donovan.westwater@gmail.com>
2019-10-16 00:21:02 -0400

    All the interactibles are in place! Now to Turn everything into prefabs and start making the level system!

---
+++ Assets/WeightSensor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeightSensor : baseTrigger
+{
+    public GameObject objectStored;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(objectStored != null && objectStored.activeSelf == false)
+        {
+            hasTriggerd = true;
+        }
+        else
+        {
+            hasTriggerd = false;
+        }
+
+    }
+    public override bool checkTrigger()
+    {
+        return hasTriggerd;
+    }
+}
