3a54e1c3b78233bb8f1c26829ae6c6c014ab6c28
donovan-westwater <Donovan.westwater@gmail.com>
2019-10-22 00:21:51 -0400

    Added all the prefabs based on the designs in the design doc. Started working on the level generatior. The level gerneation part is not imlemented in the script yet. That is what I will be working on.

---
+++ Assets/Level Modules/RoomStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomStats : MonoBehaviour
+{
+    //SHOULD ONLY INCULDE NUMBERS AND STATS, NO CODE IS REALLY NESSESARY
+    public int mana;
+    public string skill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    //SET IN EDITOR
+    public GameObject[] traversables;
+    public GameObject[] puzzles;
+    public GameObject[] obstacles;
+    List<Transform> puzzlePoints;
+    List<Transform> obstaclePoints;
+    int totalMana = 0;
+    //Place here a dicitonary of skills as key and amount of of skill as value. This should be public
+    public Dictionary<string, int> skillCount; // skill names Teleport, Guard, Partol, Grab,Move
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0, traversables.Length - 1);
+        GameObject travs = Instantiate(traversables[rand], this.transform.position, this.transform.rotation);
+        foreach (Transform child in transform.Find("PuzzlePoints"))
+        {
+            puzzlePoints.Add(child);
+           // totalMana += child.GetComponent<RoomStats>().mana;
+        }
+        foreach (Transform child in transform.Find("ObstaclePoints"))
+        {
+            obstaclePoints.Add(child);
+        }
+        //Spawning rooms
+        foreach(Transform point in puzzlePoints)
+        {
+
+        }
+        foreach (Transform point in obstaclePoints)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
